5d4cd60fb44d90c8bf15ce610805360acdb18eca
Miroslav Lalev <miro@Miroslavs-MacBook-Pro.local>
2017-01-23 23:45:48 +0200

    Improved engine and added xslt

--- com.hr.manager/src/main/java/com/hr/manager/engine/ProcessorEngine.java
+++ com.hr.manager/src/main/java/com/hr/manager/engine/ProcessorEngine.java
@@ -1,16 +1,31 @@
 package com.hr.manager.engine;
 
+import static java.nio.file.Files.deleteIfExists;
+import static org.apache.xmlgraphics.util.MimeConstants.MIME_PDF;
+
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.xml.sax.SAXException;
 
 public class ProcessorEngine {
 
 	private static final String XSLT_RESOURCE = "employee_transformer.xslt";
+	private static final String TEMP_FILE = "temp.fo";
 
 	String inputResource;
 	String outputUrl;
@@ -20,19 +35,29 @@ public class ProcessorEngine {
 		this.outputUrl = outputUrl;
 	}
 
-	public void process() {
-		try {
-			Transformer transformer = createTransformer();
+	public void process() throws IOException {
+		File tempFile = new File(TEMP_FILE);
+		try(BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(outputUrl)))) {
+			Transformer transformer = createTransformer(new File(getResourcePath(XSLT_RESOURCE)));
 			transformer.transform(new StreamSource(new File(getResourcePath(inputResource))),
-					new StreamResult(new File(outputUrl)));
-		} catch (TransformerException e) {
+					new StreamResult(tempFile));
+
+			Fop fop = createFop(stream);
+			transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.transform(new StreamSource(tempFile), new SAXResult(fop.getDefaultHandler()));
+		} catch (Exception e) {
 			throw new RuntimeException(e);
+		} finally {
+			deleteIfExists(tempFile.toPath());
 		}
 	}
 
-	private Transformer createTransformer() throws TransformerException {
-		return TransformerFactory.newInstance()
-				.newTransformer(new StreamSource(new File(getResourcePath(XSLT_RESOURCE))));
+	private Fop createFop(BufferedOutputStream stream) throws FOPException, SAXException, IOException {
+		return FopFactory.newInstance(new File(getResourcePath("fo.xconf"))).newFop(MIME_PDF, stream);
+	}
+
+	private Transformer createTransformer(File file) throws TransformerException {
+		return TransformerFactory.newInstance().newTransformer(new StreamSource(file));
 	}
 
 	private String getResourcePath(String resource) {
--- com.hr.manager/src/main/java/com/hr/manager/main/Main.java
+++ com.hr.manager/src/main/java/com/hr/manager/main/Main.java
@@ -1,13 +1,13 @@
 package com.hr.manager.main;
 
+import java.io.IOException;
+
 import com.hr.manager.engine.ProcessorEngine;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO: create xslt resource and fill test.xml correctly
-		// Currently hardcoded my desktop as output file location
-		ProcessorEngine engine = new ProcessorEngine("test.xml", "~/Desktop/test.pdf");
+	public static void main(String[] args) throws IOException {
+		ProcessorEngine engine = new ProcessorEngine("test.xml", "test.pdf");
 		engine.process();
 	}
 }
